ec7ce26264b228db1055bcc81f2894e969dfe5b4
Cleo Falvey <46943386+chfal@users.noreply.github.com>
2020-07-22 20:48:49 -0400

    nightmare code that should never run

--- quick_dirty_analyses.R
+++ quick_dirty_analyses.R
@@ -10,8 +10,10 @@ library(gganimate) # for animating / saving plots
 library(R0) #for calculating R0
 
 # Get data i hate this package
-google_mobility <- covid19mobility::refresh_covid19mobility_google_us_counties()
+# google_mobility <- covid19mobility::refresh_covid19mobility_google_us_counties()
+# write_csv(google_mobility, "google_mobility.csv")
 
+google_mobility <- read_csv("google_mobility.csv")
 #clean google data
 google_mobility <- google_mobility %>%
   group_by(state, date, data_type) %>%
@@ -20,23 +22,26 @@ google_mobility <- google_mobility %>%
   filter(date>="2020-02-15")
 
 #Get apple data from the covid19mobility package.
-apple_mobility <- covid19mobility::refresh_covid19mobility_apple_subregion()
+# apple_mobility <- covid19mobility::refresh_covid19mobility_apple_subregion()
+# write_csv(apple_mobility, "apple_mobility.csv")
 
+apple_mobility <- read_csv("apple_mobility.csv")
 #Clean apple data
+
 apple_mobility <- apple_mobility %>%
   filter(country=="United States") %>%
-  dplyr::select(c(2,3,7,8)) %>%
+  dplyr::select(c(1,2,6,7)) %>%
   rename("state"="location") %>%
-  filter(date>="2020-02-14")
+  filter(date>="2020-02-15")
 
 #Get data by every week, starting from the 15th of February. We do this by adding a "start date" column.
 
 google_mobility$start_date = cut(google_mobility$date, breaks="7 days")
 apple_mobility$start_date = cut(apple_mobility$date, breaks="7 days")
 
-final_mobility_google <- group_by(google_mobility, state, start_date) %>% summarize_if(is.numeric, mean, na.rm=T)
+google_mobility <- group_by(google_mobility, state, start_date) %>% summarize_if(is.numeric, mean, na.rm=T)
 
-final_mobility_apple <- group_by(apple_mobility, state, start_date, data_type) %>% summarize_if(is.numeric, mean, na.rm=T)
+apple_mobility <- group_by(apple_mobility, state, start_date, data_type) %>% summarize_if(is.numeric, mean, na.rm=T)
 
 #original data from Johns Hopkins
 original_data <- read_csv("time_series_covid19_confirmed_US.csv")
@@ -52,7 +57,7 @@ cases_with_pop <- left_join(original_data, pop_est) %>%
   summarize_if(is.numeric, sum, na.rm=TRUE)
 
 #selecting only the case counts, population data, and state to make a new matrix
-case_counts_raw = cases_with_pop[,30:183]
+case_counts_raw = cases_with_pop[,30:188]
 case_counts_raw <- as.matrix(case_counts_raw)
 rownames(case_counts_raw) = cases_with_pop$Province_State
 
@@ -76,7 +81,7 @@ mGT = generation.time("gamma", c(3, 1.5))
 case_counts[case_counts<0]=0
 
 # removed all cases where you didn't know what the population was
-case_counts <- case_counts[case_counts[,153] !=0,]
+case_counts <- case_counts[case_counts[,158] !=0,]
 
 weekly <- seq(0,ncol(case_counts), by=7)
 
@@ -95,7 +100,7 @@ for (i in 1:nrow(case_counts)){ # we are going to iterate over all the states
       R0=NA
     }
     else{
-      R0EG = estimate.R(case_counts[i,], GT=mGT,  methods=c('EG'), pop.size = case_counts[,145], nsim=10, begin=weekly[j]+1, end=weekly[j+1])
+      R0EG = estimate.R(case_counts[i,], GT=mGT,  methods=c('EG'), pop.size = case_counts[,158], nsim=10, begin=weekly[j]+1, end=weekly[j+1])
       R0 = R0EG$estimates$EG$R
     }
     r0[l,1]=rownames(case_counts)[i]
@@ -109,12 +114,12 @@ r0 <- r0 %>%
   tibble()
 
 #cut the result to quantiles - remove anything in the 95th percentile and the 5th percentile
-r0$R0[r0$R0>quantile(r0$R0, .95, na.rm=T)] <- NA
-r0$R0[r0$R0<quantile(r0$R0, .05, na.rm=T)] <- NA
+# r0$R0[r0$R0>quantile(r0$R0, .95, na.rm=T)] <- NA
+# r0$R0[r0$R0<quantile(r0$R0, .05, na.rm=T)] <- NA
 
 #add labels to each week's start date
-Week <- as.character(c(1:21))
-dates <- colnames(case_counts[,c(1,8,15,22,29,36,43,50,57,64,71,78,85,92,99,106,113,120,127,134, 141)])
+Week <- as.character(c(1:22))
+dates <- colnames(case_counts[,c(1,8,15,22,29,36,43,50,57,64,71,78,85,92,99,106,113,120,127,134, 141,148)])
 
 positions <- data.frame(Week, dates)
 
@@ -126,11 +131,11 @@ r0 <- read_csv("r0_result_data_frame.csv") %>%
   mutate(date=as.Date(dates, "%m/%d/%y")) %>%
   dplyr::select(c("State","R0","date"))
 
-final_mobility_apple <-final_mobility_apple %>%
+final_mobility_apple <-apple_mobility %>%
   mutate(start_date=as.Date(start_date)) %>%
   dplyr::select(c("start_date","data_type","value"))
 
-final_mobility_google <-final_mobility_google %>%
+final_mobility_google <-google_mobility %>%
   mutate(start_date=as.Date(start_date))
 
 corr_apple <- left_join(final_mobility_apple, r0, by =c("start_date"="date", "state"="State"))
@@ -156,7 +161,8 @@ weather <- weather %>%
 
 weather$start_date <- as.Date(weather$start_date, "%Y-%m-%d")
 
-weather_total <- weather %>% dplyr::select("Province_State", "start_date", "temp_in_K")
+weather_total <- weather %>% dplyr::select("Province_State", "start_date", "temp_in_K") %>%
+  mutate(temp_in_c=temp_in_K - 273.15)
 
 #read in mask data
 masks <- read_csv("maskdata.csv") %>%
@@ -174,7 +180,7 @@ corr_apple2$masks_worn <- (corr_apple2$start_date > corr_apple2$mask_date)
 
 #if all NA that means masks were never worn, so set it to false
 corr_google2[,11][is.na(corr_google2[,11])] <- FALSE
-corr_apple2[,7][is.na(corr_apple2[,7])] <- FALSE
+corr_apple2[,6][is.na(corr_apple2[,6])] <- FALSE
 
 final_google_corr <- left_join(corr_google2, weather_total, by=c("state"="Province_State", "start_date"="start_date"))
 write_csv(final_google_corr, "final_google_corr.csv")
@@ -199,8 +205,8 @@ for (i in 1:nrow(case_counts)){ # we are going to iterate over all the states
   }
 }
 
-Week <- as.character(c(1:21))
-dates <- colnames(case_counts[,c(1,8,15,22,29,36,43,50,57,64,71,78,85,92,99,106,113,120,127,134, 141)])
+Week <- as.character(c(1:22))
+dates <- colnames(case_counts[,c(1,8,15,22,29,36,43,50,57,64,71,78,85,92,99,106,113,120,127,134, 141, 148)])
 
 positions <- data.frame(Week, dates)
 
@@ -226,7 +232,7 @@ write_csv(final_apple_corr2, "final_apple_corr.csv")
 total_data <- left_join(final_apple_corr2, final_google_corr2)
 
 #all the google mobility that should decrease (i.e. everything but housing)
-total_data$expected_decrease <- base::rowMeans(total_data[,c(17,16,14,13,12,11)], na.rm=T)
+total_data$expected_decrease <- base::rowMeans(total_data[,c(16,14,13,12,11)], na.rm=T)
 
 #lag data
 total_data <- total_data %>%
@@ -238,8 +244,4 @@ total_data <- total_data %>%
 total_data <- total_data %>%
   mutate_if(is.numeric, list(~na_if(., Inf)))
 
-total_data$pct_change[total_data$pct_change>quantile(total_data$pct_change, .95, na.rm=T)] <- NA
-total_data$pct_change[total_data$pct_change<quantile(total_data$pct_change, .05, na.rm=T)] <- NA
-
 write_csv(total_data, "total_joined_data.csv")
-s
